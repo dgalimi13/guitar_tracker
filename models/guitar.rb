84f37eea7dce3e151af2daa191bd49589713c449
danny galimi <dannygalimi13@aim.com>
2022-07-02 21:31:44 -0400

    added guitar model with attr_accessor attributes and initialize method

---
+++ models/guitar.rb
@@ -0,0 +1,13 @@
+class Guitar
+    attr_accessor :type, :model, :brand
+
+
+
+    def initialize(type,model,brand)
+        @type = type
+        @model = model 
+        @brand = brand
+    end 
+
+
+end 
