1cd0c725bfb0f61f75f5ca84e96052cb504dbc1a
danny galimi <dannygalimi13@aim.com>
2022-07-15 20:15:24 -0400

    added post /users and post /login routes to users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+
+    get '/register' do
+        erb :'users/new.html'
+    end 
+
+    post '/users' do
+        @user = User.new
+        @user.email = params[:email]
+        @user.password = params[:password]
+
+        if @user.save 
+            redirect "/"
+        else
+            erb :'users/new.html'
+        end
+    end
+
+    get '/login' do 
+        erb :'users/login.html'
+    end 
+
+    post '/login' do
+        @user = User.find_by(:email => params[:email])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+          redirect "/"
+        else
+          erb :'users/login.html'
+        end
+    end
+
+    get '/logout' do 
+        session.clear
+        redirect '/'
+    end
+ 
+
+
+end   
