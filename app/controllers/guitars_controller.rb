d597a176f2d5b4adf892e352dcd7685800a38936
danny galimi <dannygalimi13@aim.com>
2022-08-04 21:04:34 -0400

    add delete route to Guitars contoller. delete route will find guitar by id, destroy guitar and redirect to brand index page

--- app/controllers/guitars_controller.rb
+++ app/controllers/guitars_controller.rb
@@ -3,7 +3,7 @@ class GuitarsController < ApplicationController
     delete "/guitars/:id" do
     @guitar = guitar.find_by(id: params[:id])
     @guitar.destroy
-    redirect to "/brands/#{ @guitar.brands.id }"
+    redirect to "/brands/#{ @guitar.brand.id }"
     end
     
     
