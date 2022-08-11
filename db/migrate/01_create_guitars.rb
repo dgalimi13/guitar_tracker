6f829bf551937c378545a1c81a36c4032f73fb2f
danny galimi <dannygalimi13@aim.com>
2022-08-11 19:06:49 -0400

    change :coupons to :guitars in create_guitars migration

--- db/migrate/01_create_guitars.rb
+++ db/migrate/01_create_guitars.rb
@@ -1,6 +1,6 @@
 class CreateGuitars < ActiveRecord::Migration[6.1] 
     def change 
-        create_table :coupons do |t|
+        create_table :guitars do |t|
             t.string :type
             t.string :model
             t.string :brand 
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2) do
+
+  create_table "coupons", force: :cascade do |t|
+    t.string "type"
+    t.string "model"
+    t.string "brand"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+  end
 
 end
